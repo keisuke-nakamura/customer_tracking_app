a956852967fd24710fb3818a4967f7209f399a48
keisuke-nakamura <35714898+keisuke-nakamura@users.noreply.github.com>
2020-02-07 00:50:16 +0900

    2

--- tracking/views.py
+++ tracking/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import pdb
 
 
 def login(request):
+    print(request)
     if request.method == 'POST':
         return render(request, 'main.html')
 
-    return render(request, 'main.html')
-    # return render(request, 'login.html')
+    return render(request, 'login.html')
 
 
 def main(request):
