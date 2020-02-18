daccb055b519145ea8630b5feca0010158c73184
keisuke-nakamura <kei9361@gmail.com>
2020-02-18 17:30:39 +0900

    mock app 20%

--- tracking/urls.py
+++ tracking/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('index/', views.index, name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('unassigned/', views.unassigned, name='unassigned'),
+    path('how_to_use/', views.how_to_use, name='how_to_use'),
+    path('tour/', views.tour, name='tour'),
 ]
--- tracking/views.py
+++ tracking/views.py
@@ -4,9 +4,26 @@ from django.urls import reverse
 
 
 def login(request):
-
     return render(request, 'login.html')
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def registration(request):
+    return render(request, 'customer_registration.html')
+
+
+def unassigned(request):
+    return render(request, 'unassigned.html')
+
+
+def how_to_use(request):
+    return render(request, 'how_to_use.html')
+
+
+def tour(request):
+    return render(request, 'tour.html')
+
 
