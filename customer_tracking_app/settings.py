09f22f1c7d3557fcdbb78c23de56cdf818b9f4c1
keisuke-nakamura <kei9361@gmail.com>
2020-03-16 17:39:49 +0900

    add_allowed_host

--- customer_tracking_app/settings.py
+++ customer_tracking_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e-xn4wa!g+cn)586t#ebf7^((lrb4m-uhcgxb^%hs=6_3c^k(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
